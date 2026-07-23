0ddbe1abb467de4d9f7f6bcc4a0e2975c4a31e0c
ibnuena <71430096+ibnuena@users.noreply.github.com>
2026-07-23 22:49:36 +0900

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -2,7 +2,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 	
-      System.out.println("ddd12312ddd3 20260723");
+      System.out.println("ddd12312ddd3 20260723 d");
 	}
 
 }
